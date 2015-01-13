29c8bab36dc62397df484ca86a82af906a44144e
Ekrem Doğan <Ekrem@Ekrem-MacBook-Pro.local>
2015-01-13 11:49:04 +0200

    Last commit

--- weatherCheck/ViewController.swift
+++ weatherCheck/ViewController.swift
@@ -10,6 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var city: UITextField!
+    @IBOutlet var message: UILabel!
+    @IBAction func buttonPressed(sender: AnyObject) {
+        
+        var urlString = "http://www.weather-forecast.com/locations/" + city.text.stringByReplacingOccurrencesOfString(" ", withString: "") + "/forecasts/latest"
+        var url = NSURL(string: urlString)
+        
+        
+        let session = NSURLSession.sharedSession()
+        let task = session.dataTaskWithURL(url!) {(data, response, error) in
+            if error == nil {
+                var html = NSString(data: data, encoding: NSUTF8StringEncoding)
+                if ((html?.containsString("<span class=\"phrase\">")) != false) {
+                    
+                    var htmlArray = html?.componentsSeparatedByString("<span class=\"phrase\">")
+                    
+                    var firstHalf = htmlArray?[1] as? NSString
+                    var secondHalf = firstHalf?.componentsSeparatedByString("</span>")
+                    var result = secondHalf?[0].stringByReplacingOccurrencesOfString("&deg;", withString: "°")
+                    dispatch_async(dispatch_get_main_queue(), {
+                        self.message.text = result
+                    })
+                    
+                }
+                else {
+                    dispatch_async(dispatch_get_main_queue(), {
+                        self.message.text = "couldn't find city, try again."
+                    })
+                }
+            }
+        }
+        
+        task.resume()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
