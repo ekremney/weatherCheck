43ccca778d2d73a2930be846298fb71f700e0aba
Ekrem Doğan <Ekrem@Ekrem-MacBook-Pro.local>
2015-01-13 10:15:36 +0200

    Initial Commit

---
+++ weatherCheck/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  weatherCheck
+//
+//  Created by Ekrem Doğan on 13.01.2015.
+//  Copyright (c) 2015 Ekrem Doğan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
